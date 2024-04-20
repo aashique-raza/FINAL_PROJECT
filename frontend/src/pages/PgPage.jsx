75e4018f4860d177cafb2f1e2e656b5ffffd22a3
farhan ansari <farhanraza2239@gmail.com>
2024-04-21 02:44:14 +0530

    complete pg room details with responisve

--- frontend/src/pages/PgPage.jsx
+++ frontend/src/pages/PgPage.jsx
@@ -1,72 +1,134 @@
 import React, { useState } from "react";
 import "../styles/Pg.css";
-import { sharingOptions,kitchenOptions,balconyOptions } from "../utils";
+import {
+  sharingOptions,
+  kitchenOptions,
+  balconyOptions,
+  rentAmountOptions,
+  roomAmenities,
+} from "../utils";
 
 function PgPage() {
+  const [formData, setFormData] = useState({
+    roomAmenities:[]
+  });
 
-  const[formData,setFormData]=useState({})
+  const[amenities,setAmenities]=useState([])
 
-  const handleChange=(e)=>{
-    const {id,value}=e.target;
+  const handleChange = (e) => {
+    const { id, value } = e.target;
     setFormData({
       ...formData,
-      [id]:value
+      [id]: value,
+    });
+  };
+
+  const handleCheckboxChange = (event, id) => {
+    const { checked } = event.target;
+
+    if (checked) {
+      // Add the checked amenity to the roomAmenities array
+      setFormData((prevData) => ({
+        ...prevData,
+        roomAmenities: [...prevData.roomAmenities, id],
+      }));
+    } else {
+      // Remove the unchecked amenity from the roomAmenities array
+      setFormData((prevData) => ({
+        ...prevData,
+        roomAmenities: prevData.roomAmenities.filter((amenityId) => amenityId !== id),
+      }));
     }
-    )
-  }
-
+  };
   // console.log(formData)
-
+  // console.log(amenities)
 
   return (
     <main className="pg-container">
-      <section className="pg-section-1">
-        <div className="pg-section-heading">
-          <h1>Living Space Details: </h1>
-          <p>Delve into Comfort, Your Living Space Unraveled!</p>
-        </div>
-        <div className="room-basic-details">
-          <form action="" className="room-form">
-          <div className="room-items">
-            <p>sharing:</p>
+      <form action="">
+        <section className="pg-section-1">
+          <div className="pg-section-heading">
+            <h1>Living Space Details: </h1>
+            <p>Delve into Comfort, Your Living Space Unraveled!</p>
+          </div>
 
-            <select
-              name="select-item"
-              id="roomSharing"
-              className="sharing-items"
-              value={formData.roomSharing}
-              onChange={handleChange}
-            >
-              {sharingOptions?.map((option,index) => (
-                <option value={option.value} key={index} >{option.label}</option>
+          <div className="room-basic-details">
+            <div className="room-details-1">
+              <div className="room-items">
+                <p>sharing:</p>
+
+                <select
+                  name="select-item"
+                  id="roomSharing"
+                  className="sharing-items"
+                  value={formData.roomSharing}
+                  onChange={handleChange}
+                >
+                  {sharingOptions?.map((option, index) => (
+                    <option value={option.value} key={index}>
+                      {option.label}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="room-items">
+                <p>kitchen:</p>
+                <select
+                  name="kitchen-details"
+                  id="kitchen"
+                  onChange={handleChange}
+                  value={formData.kitchen}
+                >
+                  {kitchenOptions?.map((opt, index) => (
+                    <option value={opt.value} key={index}>
+                      {opt.label}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="room-items">
+                <p>balcony:</p>
+                <select
+                  name="balcony-details"
+                  id="balcony"
+                  onChange={handleChange}
+                  value={formData.balcony}
+                >
+                  {balconyOptions?.map((opt, index) => (
+                    <option value={opt.value} key={index}>
+                      {opt.label}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+            <div className="room-details-2">
+              {rentAmountOptions?.map((amountOpt, index) => (
+                <div key={index} className="amount-details">
+                  <label htmlFor="">{amountOpt.label}</label>
+                  <input type="number"  id={amountOpt.id} placeholder={amountOpt.placeholder}  onChange={handleChange} />
+                </div>
               ))}
-            </select>
+            </div>
+            <div className="room-details-3">
+              <h3>room amenities:</h3>
+              <div className="amenities-wrapper">
+                {roomAmenities.map((ament, index) => (
+                  <div className="room-amenities" key={index}>
+                    <label htmlFor={ament.name}>{ament.name}: </label>
+                    <input
+                      type="checkbox"
+                      id={ament.name}
+                      
+                      onChange={(e) => handleCheckboxChange(e, ament.name)}
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
-          <div className="room-items">
-            <p>kitchen:</p>
-            <select name="kitchen-details" id="kitchen" onChange={handleChange} value={formData.kitchen}>
-              {
-                kitchenOptions?.map((opt,index)=>(
-                  <option value={opt.value} key={index} >{opt.label}</option>
-                ))
-              }
-            </select>
-          </div>
-          <div className="room-items">
-            <p>balcony:</p>
-            <select name="balcony-details" id="balcony" onChange={handleChange} value={formData.balcony}>
-              {
-                balconyOptions?.map((opt,index)=>(
-                  <option value={opt.value} key={index}>{opt.label}</option>
-                ))
-              }
-             
-            </select>
-          </div>
-          </form>
-         
-        </div>
-      </section>
+        </section>
+      </form>
     </main>
   );
 }
--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -1,23 +1,55 @@
+import {
+  FaCouch,
+  FaTv,
+  FaBed,
+  FaFire,
+  FaSnowflake,
+  FaBath,
+  FaFan,
+  FaRegLightbulb,
+  FaLock,
+  FaDeskpro,
+  FaWifi,
+} from "react-icons/fa";
 
-export const sharingOptions=[
-    {label:'select',value:''},
-    {label:'single',value:'single'},
-    {label:'double',value:'double'},
-    {label:'three',value:'three'},
-    {label:'four',value:'four'}
-]
+export const sharingOptions = [
+  { label: "select", value: "" },
+  { label: "single", value: "single" },
+  { label: "double", value: "double" },
+  { label: "three", value: "three" },
+  { label: "four", value: "four" },
+];
 
-export const kitchenOptions=[
-    {
-        label:"select",value:''
-    },
-    {label:'with kitchen',value:'yes kitchen'},
-    {label:'without kitchen',value:'no kitchen'},
+export const kitchenOptions = [
+  {
+    label: "select",
+    value: "",
+  },
+  { label: "with kitchen", value: "yes kitchen" },
+  { label: "without kitchen", value: "no kitchen" },
+];
 
-]
+export const balconyOptions = [
+  { label: "select", value: "" },
+  { label: "yes", value: "yes" },
+  { label: "no", value: "no" },
+];
 
-export const balconyOptions=[
-    {label:'select',value:''},
-    {label:'yes',value:'yes'},
-    {label:'no',value:'no'}
-]
+export const rentAmountOptions = [
+  { label: "expected rentAmount on per person", placeholder: "00.00",id:'rentAmount' },
+  { label: "deposit amount  on per person", placeholder: "00.00",id:'depositAmount' },
+];
+
+export const roomAmenities = [
+  { name: "cupboard",  },
+  { name: "ac"},
+  { name: "attached bathroom" },
+  { name: "fan"  },
+  { name: "TV" },
+  { name: "bedding"  },
+  { name: "geyser ",  },
+  { name: "room heater"},
+  { name: "study table" },
+  { name: "WiFi" },
+  { name: "locker"},
+];
