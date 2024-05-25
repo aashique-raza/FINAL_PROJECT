83be37fbf83d03ae3f8dad2af6b88d7f2b5c052d
farhan ansari <farhanraza2239@gmail.com>
2024-05-25 10:50:38 +0530

    property head comp responsive completed

--- frontend/src/components/SinglePropertyComp/PropertyHeadComp.jsx
+++ frontend/src/components/SinglePropertyComp/PropertyHeadComp.jsx
@@ -6,28 +6,28 @@ import { FaIndianRupeeSign } from "react-icons/fa6";
 function PropertyHeadComp() {
   return (
     <div className="property-section proeprty-section1">
-      <div>
+      <div className=" border-2  border-gray-200" >
         <h2 className=" text-5xl text-gray-500 ">
          
           <IoHomeOutline  />
         </h2>
         <p>pg</p>
       </div>
-      <div className=" border-b-2 border-gray-300 md:border-none">
+      <div className=" border-2  border-gray-200">
         <h3>3 BHK Flat In Unitech Uniworld City For Rent In Sector-30</h3>
         <p>Sector-30, Gurgaon, Haryana, INDIA.</p>
       </div>
-      <div>
+      <div className=" border-2  border-gray-200">
         <h3 className=" flex items-center">
           <FaIndianRupeeSign /> <span>80,000/m</span>
         </h3>
         <p>rent</p>
       </div>
-      <div>
+      <div className=" border-2  border-gray-200">
         <h3>230</h3>
         <p>sqrft.</p>
       </div>
-      <div>
+      <div className=" border-2  border-gray-200">
         <h3 className=" flex items-center">
           {" "}
           <FaIndianRupeeSign /> <span> 80,000</span>
