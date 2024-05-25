a0a0c7476f161122400dacc141fff2e2306ae7be
farhan ansari <farhanraza2239@gmail.com>
2024-05-25 13:54:23 +0530

    property section2 completed

--- frontend/src/components/SinglePropertyComp/ImageGalleryComp.jsx
+++ frontend/src/components/SinglePropertyComp/ImageGalleryComp.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import ImageGallery from "react-image-gallery";
-import Modal from "react-modal";
+import Modal from 'react-modal';
+
+// Set the app element
+Modal.setAppElement('#root');
 import "../../styles/SingleProperty.css";
 import "react-image-gallery/styles/css/image-gallery.css";
 
@@ -25,50 +28,43 @@ function ImageGalleryComp({ images }) {
   const remainingCount = images.length - firstThree.length; // Calculate the remaining count
   return (
     <div className="image-gallery-wrapper">
-      <div className="image-preview image-preview-large">
-        <div className="image-preview-header">
-          <button className="photos-button " onClick={openModal}>Photos</button>
-          <button className="favorite-button" onClick={toggleFavorite}>
-            <span className={`heart-icon ${isFavorite ? "favorite" : ""}`}>
-              &#x2764;
-            </span>{" "}
-            Add to favorite
-          </button>
-        </div>
-        <img src={firstThree[0].original} alt="" />
+    <div className="image-preview image-preview-large">
+      <div className="image-preview-header">
+        <button className="photos-button" onClick={openModal}>Photos</button>
+        <button className="favorite-button" onClick={toggleFavorite}>
+          <span className={`heart-icon ${isFavorite ? 'favorite' : ''}`}>&#x2764;</span> Add to favorite
+        </button>
       </div>
-      <div className="image-preview">
-        <img src={firstThree[1].original} alt="" />
-      </div>
-      <div className="image-preview relative">
-        <img src={firstThree[2].original} alt="" />
-        {remainingCount > 0 && (
-          <div className="overlay cursor-pointer" onClick={openModal}>
-            <span className="overlay-text sm:text-2xl text-xl">{`+${remainingCount} more`}</span>
-          </div>
-        )}
-      </div>
-      {/* Modal for Image Gallery */}
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        
-      >
-        <button onClick={closeModal}>Close</button>
-        <div
-          style={{ maxWidth: "100%", maxHeight: "100%", overflow: "hidden" }}
-        >
-          <ImageGallery
-            items={images.map((image) => ({
-              original: image.original,
-              originalClass: "modal-image",
-            }))}
-            showPlayButton={false} // Optional: Hide play button
-            showThumbnails={false} // Optional: Hide thumbnail navigation
-          />
-        </div>
-      </Modal>
+      <img src={firstThree[0]} alt="" />
     </div>
+    <div className="image-preview">
+      <img src={firstThree[1]} alt="" />
+    </div>
+    <div className="image-preview relative">
+      <img src={firstThree[2]} alt="" />
+      {remainingCount > 0 && (
+        <div className="overlay cursor-pointer" onClick={openModal}>
+          <span className="overlay-text sm:text-2xl text-xl">{`+${remainingCount} more`}</span>
+        </div>
+      )}
+    </div>
+    {/* Modal for Image Gallery */}
+    <Modal
+      isOpen={modalIsOpen}
+      onRequestClose={closeModal}
+    >
+      <button onClick={closeModal}>Close</button>
+      <div
+        style={{ maxWidth: "100%", maxHeight: "100%", overflow: "hidden" }}
+      >
+        <ImageGallery
+          items={images.map(image => ({ original: image, originalClass: "modal-image" }))}
+          showPlayButton={false} // Optional: Hide play button
+          showThumbnails={false} // Optional: Hide thumbnail navigation
+        />
+      </div>
+    </Modal>
+  </div>
   );
 }
 
--- frontend/src/pages/PropertyPage.jsx
+++ frontend/src/pages/PropertyPage.jsx
@@ -6,7 +6,20 @@ import "../styles/SingleProperty.css";
 import PropertyHeadComp from "../components/SinglePropertyComp/PropertyHeadComp";
 import ImageGalleryComp from "../components/SinglePropertyComp/ImageGalleryComp";
 import FacilityItem from "../components/FacilityItem";
-import { FaBed, FaBuilding, FaMale, FaFemale, FaUser, FaKey, FaBicycle, FaCar, FaCalendarAlt, FaWrench, FaTree } from 'react-icons/fa';
+import {
+  FaBed,
+  FaBuilding,
+  FaMale,
+  FaFemale,
+  FaUser,
+  FaKey,
+  FaBicycle,
+  FaCar,
+  FaCalendarAlt,
+  FaWrench,
+  FaTree,
+} from "react-icons/fa";
+import { AiFillMessage } from "react-icons/ai";
 
 function PropertyPage() {
   const { category, id } = useParams();
@@ -14,31 +27,31 @@ function PropertyPage() {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  const iconsArray = [
+    { type: "no of bedroom", name: "2 Bedroom", icon: <FaBed /> },
+    { type: "number", name: "Building", icon: <FaBuilding /> },
+    { type: "prefered tentet", name: "Male", icon: <FaMale /> },
+
+    { type: "avaibility", name: "immidiate", icon: <FaKey /> },
+
+    { type: "parking", name: "Car parking", icon: <FaCar /> },
+    { type: "posted on", name: "24/05/2-24", icon: <FaCalendarAlt /> },
+    { type: "balcony", name: "2", icon: <FaWrench /> },
+    { type: "building age", name: "above thre years", icon: <FaTree /> },
+  ];
+
   const ImagesUrl = [
-    {
-      original:
-        "https://images.pexels.com/photos/2091634/pexels-photo-2091634.jpeg?auto=compress&cs=tinysrgb&w=600",
-    },
-    {
-      original:
-        "https://images.pexels.com/photos/36366/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600",
-    },
-    {
-      original:
-        "https://images.pexels.com/photos/2459/stairs-home-loft-lifestyle.jpg?auto=compress&cs=tinysrgb&w=600",
-    },
-    {
-      original:
-        "https://images.pexels.com/photos/2834211/pexels-photo-2834211.jpeg?auto=compress&cs=tinysrgb&w=600",
-    },
-    {
-      original:
-        "https://images.pexels.com/photos/53782/pexels-photo-53782.jpeg?auto=compress&cs=tinysrgb&w=600",
-    },
-    {
-      original:
-        "https://images.pexels.com/photos/54094/road-distance-landscape-horizon-54094.jpeg?auto=compress&cs=tinysrgb&w=600",
-    },
+    "https://images.pexels.com/photos/2091634/pexels-photo-2091634.jpeg?auto=compress&cs=tinysrgb&w=600",
+
+    "https://images.pexels.com/photos/36366/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=600",
+
+    "https://images.pexels.com/photos/2459/stairs-home-loft-lifestyle.jpg?auto=compress&cs=tinysrgb&w=600",
+
+    "https://images.pexels.com/photos/2834211/pexels-photo-2834211.jpeg?auto=compress&cs=tinysrgb&w=600",
+
+    "https://images.pexels.com/photos/53782/pexels-photo-53782.jpeg?auto=compress&cs=tinysrgb&w=600",
+
+    "https://images.pexels.com/photos/54094/road-distance-landscape-horizon-54094.jpeg?auto=compress&cs=tinysrgb&w=600",
   ];
 
   useEffect(() => {
@@ -104,57 +117,28 @@ function PropertyPage() {
     <main className="property-main-container">
       <PropertyHeadComp />
       <div className="property-section property-section2  ">
-       <ImageGalleryComp images={ImagesUrl} />
-       <aside className="property-left-side-box">
-        <div className="facilities_items px-3 flex flex-wrap  gap-5 py-3 border-2 w-full " >
-        <FacilityItem
-                icon={<FaKey />}
-                type={"avaibility"}
-                name='immidiate'
-                />
-                 <FacilityItem
-                icon={<FaBed />}
-                type={"bedroom"}
-                name='2 bedroom'
-                />
-                 <FacilityItem
-                icon={<FaBed />}
-                type={"bedroom"}
-                name='2 bedroom'
-                />
-                 <FacilityItem
-                icon={<FaBed />}
-                type={"bedroom"}
-                name='2 bedroom'
-                />
-                 <FacilityItem
-                icon={<FaBed />}
-                type={"bedroom"}
-                name='2 bedroom'
-                />
-                 <FacilityItem
-                icon={<FaBed />}
-                type={"bedroom"}
-                name='2 bedroom'
-                />
-                 <FacilityItem
-                icon={<FaBed />}
-                type={"bedroom"}
-                name='2 bedroom'
-                />
-                 <FacilityItem
-                icon={<FaBed />}
-                type={"bedroom"}
-                name='2 bedroom'
-                />
+        <ImageGalleryComp images={ImagesUrl} />
+        <aside className="property-left-side-box">
+          <div className="facilities_items px-3 lg:py-6 flex flex-wrap  gap-5 lg:gap-8 py-3 border-2 w-full ">
+            {iconsArray?.map((item, index) => (
+              <FacilityItem
+                key={index}
+                icon={item.icon}
+                type={item.type}
+                name={item.name}
+              />
+            ))}
+          </div>
+          <div className="flex items-center gap-3 justify-start w-full px-6 mt-10 border-2 border-gray-200 py-6">
+            <button className=" w-auto focus:ring-0 border-none outline-none   px-12 py-6 bg-red-600 text-white capitalize text-2xl font-roboto ">
+              get owner details
+            </button>
 
-        </div>
-        <div>
-
-        </div>
-               
-       </aside>
-       
+            <button className="bg-green-600  px-12 py-6 text-3xl sm:text-4xl text-white">
+              <AiFillMessage/>
+            </button>
+          </div>
+        </aside>
       </div>
     </main>
   );
