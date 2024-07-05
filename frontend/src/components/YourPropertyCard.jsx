f184c3b4349c4326a6ff228c52d9c7c9c7446269
farhan ansari <farhanraza2239@gmail.com>
2024-07-06 02:55:32 +0530

    inactive active property feature completed

--- frontend/src/components/YourPropertyCard.jsx
+++ frontend/src/components/YourPropertyCard.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "../styles/YourProperty.css";
 import { API_URL } from "../configue";
 import { LuArrowUpRightSquare } from "react-icons/lu";
@@ -12,71 +12,117 @@ import { Button } from "flowbite-react";
 import { useSelector } from "react-redux";
 import { getTokenFromLocalStorage } from "../token";
 
-
-function YourPropertyCard({showSuccessMessage, property }) {
-
-  const[modal,setModal]=useState(false)
+function YourPropertyCard({ showSuccessMessage, property }) {
+  const [modal, setModal] = useState(false);
   const { user } = useSelector((state) => state.user);
   const token = getTokenFromLocalStorage();
-  const[error,setError]=useState(null)
-  const[loading,setLoading]=useState(false)
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
+  const [propertyActivate, setPropertyActivate] = useState(false);
+  const [activateCompleted, setActivateCompleted] = useState(false);
 
-  const handleDeleteProperty=async(cat,id)=>{
+  const handleDeleteProperty = async (cat, id) => {
     // console.log(cat,id)
     // alert('working...',)
-    let url
-    if(cat==='pg'){
-        url=`${API_URL}/pg/propertyDelete/${id}/${user._id}`
-    }else{
-        url=`${API_URL}/rent/propertyDelete/${id}/${user._id}`
+    let url;
+    if (cat === "pg") {
+      url = `${API_URL}/pg/propertyDelete/${id}/${user._id}`;
+    } else {
+      url = `${API_URL}/rent/propertyDelete/${id}/${user._id}`;
     }
 
     // console.log('url',url)
 
     try {
-      setError(null)
-      setLoading(true)
+      setError(null);
+      setLoading(true);
 
-      
-    const resp=await fetch(url,{
-      method:'DELETE',
-      headers: {
-        // "Content-Type": "multipart/form-data"// JSON format mein Content-Type header set kiya gaya hai
-        "Authorization": `Bearer ${token}`
-      },
-      credentials:'include',
-      
-    })
+      const resp = await fetch(url, {
+        method: "DELETE",
+        headers: {
+          // "Content-Type": "multipart/form-data"// JSON format mein Content-Type header set kiya gaya hai
+          Authorization: `Bearer ${token}`,
+        },
+        credentials: "include",
+      });
 
-    const result=await resp.json()
-    // console.log(result)
+      const result = await resp.json();
+      // console.log(result)
 
-    if(!resp.ok){
-      setError(result.message)
-      setLoading(false)
-      return
-    }
+      if (!resp.ok) {
+        setError(result.message);
+        setLoading(false);
+        return;
+      }
 
-    setModal(false)
-    showSuccessMessage('deleted successfully!')
-
-
-      
+      setModal(false);
+      showSuccessMessage("deleted successfully!");
     } catch (error) {
-      console.log('delete property failed',error)
-      setError(error.message)
-      setModal(false)
+      console.log("delete property failed", error);
+      setError(error.message);
+      setModal(false);
+    }
+  };
+
+  const handlePropertyActivation = async (cat, id) => {
+    let url;
+    if (cat === "pg") {
+      url = `${API_URL}/pg/activateProperty/${id}/${user._id}`;
+      // /activateProperty/:propertyId/:userid
+    } else {
+      url = `${API_URL}/rent/activateProperty/${id}/${user._id}`;
     }
 
+    // console.log('url',url)
 
-  }
+    try {
+      setError(null);
+      setLoading(true);
+
+      const resp = await fetch(url, {
+        method: "PATCH",
+        headers: {
+          // "Content-Type": "multipart/form-data"// JSON format mein Content-Type header set kiya gaya hai
+          Authorization: `Bearer ${token}`,
+        },
+        credentials: "include",
+      });
+
+      const result = await resp.json();
+      // console.log(result)
+
+      if (!resp.ok) {
+        setError(result.message);
+        setLoading(false);
+        setPropertyActivate(false);
+        return;
+      }
+
+      setPropertyActivate(false);
+      setActivateCompleted(!activateCompleted)
+      activateCompleted?showSuccessMessage("Deactivated successfully!"):showSuccessMessage("activated successfully!")
+      
+
+    } catch (error) {
+      console.log("activate property failed", error);
+      setError(error.message);
+      setPropertyActivate(false);
+    }
+  };
 
   return (
     <div className="your-property-card shadow-xl hover:shadow-2xl transition-all duration-100 ease-in">
       <div className="flex justify-end items-center">
-        <button className="py-3 px-6 sm:py-4   lg:px-13 capitalize text-sm sm:text-xl lg:text-2xl tracking-wider text-slate-700 bg-gray-100">
-          inactive
+        <button
+          onClick={() => setPropertyActivate(true)}
+          className={`py-3 px-6 sm:py-4 lg:px-13 capitalize text-sm sm:text-xl lg:text-2xl tracking-wider ${
+            activateCompleted
+              ? "bg-green-500 text-white"
+              : "text-slate-700 bg-gray-100"
+          }`}
+        >
+         {activateCompleted?'active':'inactive'} 
         </button>
       </div>
       <div className=" mt-4">
@@ -132,13 +178,21 @@ function YourPropertyCard({showSuccessMessage, property }) {
         </h4>
       </div>
       <div className=" flex justify-between items-center mb-10">
-        <NavLink to={`/edit-property/${property.roomSharing?'pg':"rental"}/${property._id}`} className="  tracking-wider flex justify-center items-start gap-1 lowercase  font-raleway text-xl sm:text-2xl px-5 py-3 bg-gray-200 text-teal-800 ">
+        <NavLink
+          to={`/edit-property/${property.roomSharing ? "pg" : "rental"}/${
+            property._id
+          }`}
+          className="  tracking-wider flex justify-center items-start gap-1 lowercase  font-raleway text-xl sm:text-2xl px-5 py-3 bg-gray-200 text-teal-800 "
+        >
           {" "}
           edit <MdEdit className=" inline-block text-green-700   mt-1" />{" "}
         </NavLink>
-        <button onClick={(()=>setModal(true))} className=" tracking-wider flex justify-center items-start gap-1 lowercase  font-raleway text-xl sm:text-2xl px-5 py-3 bg-gray-200 text-teal-800 ">
+        <button
+          onClick={() => setModal(true)}
+          className=" tracking-wider flex justify-center items-start gap-1 lowercase  font-raleway text-xl sm:text-2xl px-5 py-3 bg-gray-200 text-teal-800 "
+        >
           {" "}
-          delete <MdDelete  className=" inline-block  text-red-500   mt-1" />{" "}
+          delete <MdDelete className=" inline-block  text-red-500   mt-1" />{" "}
         </button>
       </div>
       <div className=" flex justify-start items-start px-3 w-full">
@@ -146,34 +200,73 @@ function YourPropertyCard({showSuccessMessage, property }) {
           0 contated
         </h3>
       </div>
-      {
-        modal && (
-          <Modal show={modal} size="md" onClose={() => setModal(false)} popup>
-        <Modal.Header />
-        <Modal.Body>
-          <div className="text-center">
-            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
-            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-              Are you sure you want to delete this property?
-            </h3>
-            <div className="flex justify-center gap-4">
-              <Button color="failure" onClick={() => 
-                {
-                  let category=property.roomSharing?'pg':'rental'
-                  handleDeleteProperty(category,property._id)
-                }
-              }>
-                {"Yes, I'm sure"}
-              </Button>
-              <Button color="gray" onClick={() => setModal(false)}>
-                No, cancel
-              </Button>
+      {modal && (
+        <Modal show={modal} size="md" onClose={() => setModal(false)} popup>
+          <Modal.Header />
+          <Modal.Body>
+            <div className="text-center">
+              <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+              <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                Are you sure you want to delete this property?
+              </h3>
+              <div className="flex justify-center gap-4">
+                <Button
+                  color="failure"
+                  onClick={() => {
+                    let category = property.roomSharing ? "pg" : "rental";
+                    handleDeleteProperty(category, property._id);
+                  }}
+                >
+                  {"Yes, I'm sure"}
+                </Button>
+                <Button color="gray" onClick={() => setModal(false)}>
+                  No, cancel
+                </Button>
+              </div>
             </div>
-          </div>
-        </Modal.Body>
-      </Modal>
-        )
-      }
+          </Modal.Body>
+        </Modal>
+      )}
+     {propertyActivate && (
+  <Modal
+    show={propertyActivate}
+    size={window.innerWidth >= 768 ? 'lg' : 'md'}
+    onClose={() => setPropertyActivate(false)}
+    popup
+  >
+    <Modal.Header />
+    <Modal.Body>
+      <div className="text-center">
+        <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+        <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+          Are you sure you want to {activateCompleted ? 'deactivate' : 'activate'} this property?
+        </h3>
+        <div className="flex justify-center gap-4">
+          <Button
+          size="lg"
+            className="text-xl md:text-2xl font-bold font-roboto py-2 px-4 md:py-3 md:px-6"
+            color="failure"
+            onClick={() => {
+              let category = property.roomSharing ? "pg" : "rental";
+              handlePropertyActivation(category, property._id);
+            }}
+          >
+            Yes, {activateCompleted ? 'deactivate' : 'activate'}
+          </Button>
+          <Button
+          size="lg"
+            className="text-xl md:text-2xl font-roboto font-bold py-2 px-4 md:py-3 md:px-6"
+            color="gray"
+            onClick={() => setPropertyActivate(false)}
+          >
+            Not Now
+          </Button>
+        </div>
+      </div>
+    </Modal.Body>
+  </Modal>
+)}
+
     </div>
   );
 }
