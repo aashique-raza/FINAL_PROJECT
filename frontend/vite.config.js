72d7fd6b129ad46c8e996191deb0acdee804e212
farhan ansari <farhanraza2239@gmail.com>
2024-07-16 10:35:49 +0530

    chnage in vite configue file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist'  // Ensure this is correctly set
+  },
   server: {
     // proxy: {
     //   '/api': {
