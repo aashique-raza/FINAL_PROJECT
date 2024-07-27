a270e613fc941fbd9e901cde5c55c9d78f5daaf4
farhan ansari <farhanraza2239@gmail.com>
2024-07-27 20:51:42 +0530

    simillar properties is working good

--- frontend/src/components/SinglePropertyComp/PropertySimillarComp.jsx
+++ frontend/src/components/SinglePropertyComp/PropertySimillarComp.jsx
@@ -4,12 +4,14 @@ import { LuIndianRupee } from "react-icons/lu";
 import "../../styles/SingleProperty.css";
 import { useParams } from "react-router-dom";
 
-function PropertySimillarComp() {
-  const { type, id } = useParams();
+function PropertySimillarComp({ data }) {
+  const { category, id } = useParams();
+  console.log(category)
 
+  console.log("bhk type", data.BHKType);
   return (
     <NavLink
-      to={`property/${type}/${data._id}`} // Update this to the actual route you want to navigate to
+      to={`property/${category}/${data._id}`} // Update this to the actual route you want to navigate to
       className="   property-simillar-box flex items-center w-full bg-white border-2 border-gray-800 shadow-lg transition transform duration-300 hover:shadow-2xl hover:scale-95"
     >
       <div className="  overflow-hidden">
@@ -21,11 +23,11 @@ function PropertySimillarComp() {
       </div>
       <div className=" duration-300  px-3 flex-1  flex flex-col items-start border-2">
         <h2 className=" hover:text-red-600  transition  ease-in-out my-2 text-xl sm:text-2xl font-medium text-slate-800 capitalize tracking-wide font-roboto">
-          {type === "rental"
+          {category === "rental"
             ? `${data.BHKType} for Rent in ${data.location.city}`
             : `${data.roomSharing} for Rent in ${data.location.city}`}
         </h2>
-       
+
         <div className="flex items-center justify-around gap-12 mt-16">
           <p className="    text-gray-900 flex flex-col items-center justify-center gap-2 capitalize font-slab text-sm sm:text-xl">
             rent
@@ -34,14 +36,14 @@ function PropertySimillarComp() {
               {data.rentAmount}
             </span>
           </p>
-          {type === "rental" && (
+          {category === "rental" && (
             <p className="  text-gray-900 flex flex-col items-center justify-center gap-2 capitalize font-slab text-sm sm:text-xl">
               build area{" "}
               <span className=" font-bold"> {data.builtUpArea} sqrft. </span>
             </p>
           )}
 
-          {type === "pg" && (
+          {category === "pg" && (
             <p className=" text-gray-900 flex flex-col items-center justify-center gap-2 capitalize font-slab text-sm sm:text-xl">
               food avaibility{" "}
               <span className="  font-bold">
